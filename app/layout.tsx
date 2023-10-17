6444ad41b8f2b0827fb5071b2d6affa0f344f656
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-18 03:16:15 +0530

    Updated metadata title and removed header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Gift it",
+  title: "gift-it",
   description:
     "Gift it is platform where you can fully customize and send gitfs to you loved ones.",
 };
@@ -18,10 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
