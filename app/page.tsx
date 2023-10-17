6987925dea7bb00f70ba9181c5a81ab7d55c2c16
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-18 00:57:57 +0530

    Removed unused html content

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
 import Image from "next/image";
+import heroImage from "@/public/bg-hero.png";
 
 export default function Home() {
-  return (
-    <main className="p-2">
-      <h1 className="text-9xl">Gift it</h1>
-      <p className="text-xl ml-3">Content on the way...</p>
-    </main>
-  );
+  return <main className=""></main>;
 }
