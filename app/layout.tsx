378ba4f916101ea608f9e512223af62fb0cafc12
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-19 00:04:15 +0530

    Updated global font from inter to raleway

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { raleway } from "./fonts";
 
 export const metadata: Metadata = {
   title: "gift-it",
@@ -17,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${raleway.className}`}>{children}</body>
     </html>
   );
 }
