191cabfac35cec14017c6a34108b7c00e18a759d
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-18 23:31:04 +0530

    Added fonts used in app

---
+++ app/fonts.ts
@@ -0,0 +1,17 @@
+import { Inter, Quicksand, Raleway } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const quicksand = Quicksand({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const raleway = Raleway({
+  weight: ["200", "300"],
+  subsets: ["latin"],
+  display: "swap",
+});
