ff26ac7b5ccaa37f0d4f930cf345dd603542fc0d
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-21 02:48:09 +0530

    Completed custom 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,34 @@
+import Navbar from "@/components/navbar";
+import { Metadata } from "next";
+import Link from "next/link";
 import React from "react";
 
-export default function NotFound() {
-  return <div>not-found</div>;
+export const metadata: Metadata = {
+  title: "404: The page could not be found.",
+};
+
+function NotFound() {
+  return (
+    <>
+      <Navbar />
+      <main className="flex flex-col items-center justify-center w-full py-12 max-w-screen-2xl">
+        <section className="flex flex-col items-center justify-center pt-[2.5rem]">
+          <h1 className="text-[10rem] md:text-[12rem]">404</h1>
+          <p className="text-center">
+            The page you are looking for does not exist. How you got here is a
+            mystery. But you can click the button below <br /> to go back to the
+            home page.
+          </p>
+          <Link
+            href="/"
+            className="flex items-center justify-center w-[8rem] h-[2.5rem] my-4 uppercase bg-black text-white font-semibold rounded-md"
+          >
+            <span>Home</span>
+          </Link>
+        </section>
+      </main>
+    </>
+  );
 }
+
+export default NotFound;
