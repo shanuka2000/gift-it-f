9691364ee895b2849ddc1f22bc6e17f7e7417c6b
shanuka2000 <shanuka.inbox@gmail.com>
2023-10-24 14:11:07 +0530

    Removed inter font

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,9 +1,4 @@
-import { Inter, Quicksand, Raleway } from "next/font/google";
-
-export const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-});
+import { Quicksand, Raleway } from "next/font/google";
 
 export const quicksand = Quicksand({
   subsets: ["latin"],
